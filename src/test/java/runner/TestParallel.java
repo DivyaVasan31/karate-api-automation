b0bde83a941bf35b4b7fed46749d45eab993912c
divyavasan.gs_automation <159038801+DivyaVasan31@users.noreply.github.com>
2026-06-11 01:45:19 +0530

    Update TestParallel.java

--- src/test/java/runner/TestParallel.java
+++ src/test/java/runner/TestParallel.java
@@ -1,1 +1,32 @@
+package runner;
 
+import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestParallel {
+
+    @Test
+    void testParallel() {
+        Results results = Runner
+            .path("classpath:features")
+            .tags("@regression")
+            .parallel(5);
+
+        assertEquals(0, results.getFailCount(),
+            results.getErrorMessages());
+    }
+
+    @Test
+    void testSmoke() {
+        Results results = Runner
+            .path("classpath:features")
+            .tags("@smoke")
+            .parallel(2);
+
+        assertEquals(0, results.getFailCount(),
+            results.getErrorMessages());
+    }
+}
