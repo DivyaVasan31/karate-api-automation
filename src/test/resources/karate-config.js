c44d33434ac85cfd677833a96dc2efa137b54300
divyavasan.gs_automation <159038801+DivyaVasan31@users.noreply.github.com>
2026-06-11 01:42:42 +0530

    Update karate-config.js

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,1 +1,23 @@
+function fn() {
+    var env = karate.env || 'dev';
+    karate.log('Environment:', env);
 
+    var config = {
+        baseUrl: 'https://dev.api.com'
+    };
+
+    if (env == 'staging') {
+        config.baseUrl = 'https://staging.api.com';
+    }
+    if (env == 'prod') {
+        config.baseUrl = 'https://api.pearson.com';
+    }
+
+    // Common headers for all requests
+    karate.configure('headers', {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    });
+
+    return config;
+}
